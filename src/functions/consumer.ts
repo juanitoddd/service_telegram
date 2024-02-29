8f985a0b6ccb151137830a954a2abe42e423fa00
Hetzner <juand.diazd@gmail.com>
2024-02-29 14:06:47 +0100

    UPDATE

--- src/functions/consumer.ts
+++ src/functions/consumer.ts
@@ -4,7 +4,7 @@ import { RabbitMsg } from '../types/rabbit'
 const consumer = (_msg: RabbitMsg) => {
     console.log('[consumer] Telegram');
     console.log(_msg);
-    bot.telegram.sendMessage(173199779, "Hello from cronicle");
+    bot.telegram.sendMessage(173199779, JSON.stringify(_msg));
 }
 
 export default consumer;
