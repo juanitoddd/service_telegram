9924ea989759b889c0b06a0cf4480cbf348c8390
Juan David <juand.diazd@gmail.com>
2024-03-02 16:37:45 +0100

    UPDATE

--- src/functions/consumer.ts
+++ src/functions/consumer.ts
@@ -1,10 +1,30 @@
 import { bot } from 'src';
 import { RabbitMsg } from '../types/rabbit'
 
+const users: number[] = [
+    173199779
+]
+
 const consumer = (_msg: RabbitMsg) => {
     console.log('[consumer] Telegram');
     console.log(_msg);
-    bot.telegram.sendMessage(173199779, JSON.stringify(_msg));
+    switch(_msg.format) {
+        case 'string':
+            send(JSON.stringify(_msg.payload))
+            break;
+        case 'json':
+            send(JSON.stringify(_msg.payload))
+            break;
+        case 'table':
+            send(JSON.stringify(_msg.payload))
+            break;
+    }    
+}
+
+const send = (_string: string) => {
+    for(const user of users) {
+        bot.telegram.sendMessage(user, _string);
+    }
 }
 
 export default consumer;
